b6a329f96b82f58d98fe278d3fb5d0e05a75f1c9
Patty Hung <pattyhung72@gmail.com>
2025-01-16 18:41:25 +1100

    Merge branch 'main' into ocr

--- frontend/src/pages/upload/Upload.tsx
+++ frontend/src/pages/upload/Upload.tsx
@@ -15,15 +15,101 @@ import { apiEndpoint } from "@/config";
 import { FormValues } from "@/types";
 
 const Upload = () => {
+  const [previewData, setPreviewData] = useState<Partial<FormValues> | null>(
+    null,
+  );
+  const [files, setFiles] = useState<File[] | null>(null);
+
+  const dropzone = {
+    accept: {
+      "image/*": [".jpg", ".jpeg", ".png"],
+    },
+    multiple: true,
+    maxFiles: 2,
+    maxSize: 1 * 1024 * 1024,
+  } satisfies DropzoneOptions;
+
+  const processFileMutation = useMutation({
+    mutationFn: async (files: File[]) => {
+      const formData = new FormData();
+      formData.append("file", files[0]);
+
+      const { data } = await axios.post(`${apiEndpoint}/process`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      console.log("Data:", data);
+      return data;
+    },
+    onSuccess: (data) => {
+      setPreviewData(data);
+    },
+    onError: (error: AxiosError) => {
+      console.error("Error processing files:", error.message);
+    },
+  });
+
+  const onsubmit = () => {
+    if (files) {
+      processFileMutation.mutate(files);
+    }
+  };
   return (
-    <div className="grid grid-cols-2 p-4">
-      <div className="p-4">
-        <h5>Upload</h5>
+    <>
+      <div className="grid grid-cols-2 p-4">
+        <div className="space-y-4 p-4">
+          <h5>Upload</h5>
+          <FileUploader
+            value={files}
+            onValueChange={setFiles}
+            dropzoneOptions={dropzone}
+            reSelect={true}
+          >
+            <FileInput>
+              <div className="flex aspect-video w-full flex-col items-center justify-center rounded-md border bg-background">
+                <CloudUpload width={60} height={60} />
+                <p className="text-gray-400">
+                  Drag & Drop or browse your image here
+                </p>
+              </div>
+            </FileInput>
+            <FileUploaderContent className="flex flex-row items-center gap-2">
+              {files?.map((file, i) => (
+                <FileUploaderItem
+                  key={i}
+                  index={i}
+                  className="size-20 overflow-hidden rounded-md p-0"
+                  aria-roledescription={`file ${i + 1} containing ${file.name}`}
+                >
+                  <img
+                    src={URL.createObjectURL(file)}
+                    alt={file.name}
+                    height={80}
+                    width={80}
+                    className="size-20 p-0"
+                  />
+                </FileUploaderItem>
+              ))}
+            </FileUploaderContent>
+          </FileUploader>
+          <Button
+            type="submit"
+            className="h-8 w-fit"
+            onClick={onsubmit}
+            disabled={processFileMutation.isPending || files === null}
+          >
+            {processFileMutation.isPending ? "Processing..." : "Preview"}
+          </Button>
+        </div>
+        {previewData && (
+          <div className="rounded-xl bg-muted/50 p-4">
+            <h5>Preview</h5>
+            <Preview previewData={previewData} />
+          </div>
+        )}
       </div>
-      <div className="rounded-xl bg-muted/50 p-4">
-        <h5>Preview</h5>
-      </div>
-    </div>
+    </>
   );
 };
 
